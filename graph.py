3ebb66634a1feeebd0212c1cbd54160a929d4fb7
Connor Bach <cbach120@gmail.com>
2018-04-09 11:08:50 -0400

    add animation

--- graph.py
+++ graph.py
@@ -1,5 +1,16 @@
 #!/usr/bin/local/python3
-import matplotlib
+import random
 
-def graphData(data):
-    print(data)
+# input: list of numbers
+# output: matplotlib plot of the numbers 
+def graphData(plt, data):
+    x_rng = [x for x in range(1,11)]
+    plt.bar(x_rng, data, align='center', alpha=0.5)
+    plt.draw()
+
+def updateGraph(plt, data):
+    print('updating graph')
+    plt.clf()
+    x_rng = [x for x in range(1,11)]
+    plt.bar(x_rng, data, align='center', alpha=0.5)
+    plt.draw()
--- main.py
+++ main.py
@@ -1,6 +1,24 @@
 #!/usr/bin/local/python3
+import graph
+import random
+import matplotlib.pyplot as plt
 
 print('Sorting Visualization')
 print('Choose Algorithm:\n1. A\n2. B\n3. C')
 
 choice = input('Enter selection: ')
+
+#set interactive
+plt.ion()
+
+#initial graph 
+nums = random.sample(range(1,100), 10)
+graph.graphData(plt, nums)
+plt.show()
+
+for x in range(1,10):
+    nums = random.sample(range(1,100), 10)
+    graph.updateGraph(plt, nums)
+    plt.pause(0.001)
+
+plt.show()
